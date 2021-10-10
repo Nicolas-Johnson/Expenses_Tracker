9a103f0e202ae0218cae633b582fb3ed28993c3e
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-10 15:01:25 -0300

    Criet AddTransaction Component and logic for add positive and negative laues

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 
 class AddTransaction extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      amount: 0,
+      entry: '',
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    if(name === 'amount') {
+      const valueToNumber = parseInt(value);
+      if(valueToNumber < 0) {
+        return this.setState({[name]: Math.abs(valueToNumber), entry: 'expense'});
+      }
+      return this.setState({[name]: valueToNumber, entry: 'income'});
+    }
+    return this.setState({[name]: value});
+  }
   render() {
+    const { title, amount } = this.state;
     return(
       <div>
         <h2>Add New Transaction</h2>
+        <div>
+          <form>
+            <label htmlFor="title">
+              Entrada/Saida:
+              <input name="title" type="text" defaultValue={ title } onChange={ this.handleChange } />
+            </label>
+            <label htmlFor="amount">
+              Valor R$ (EX: -200 = Expense / 200 = Income):
+              <input name="amount" type="number" defaultValue={ amount } onChange={ this.handleChange } />
+            </label>
+        </form>
+        </div>
       </div>
     );
   }
