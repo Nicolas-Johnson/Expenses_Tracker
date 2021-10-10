644b4f1126acc95439529ab08487e5b1976a8e66
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-10 13:27:20 -0300

    Creates History component with state

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
 class History extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      History: [{
+        title: 'salary',
+        infos: {
+          amount: 2000,
+          entry: 'income',
+        }},{
+        title: 'Books',
+        infos: {
+          amount: 200,
+          entry: 'expense',
+        }},{
+        title: 'Camera',
+        infos: {
+          amount: 300,
+          entry: 'expense',
+        }},
+      ]
+    }
+  }
   render() {
+    const { History } = this.state;
     return(
       <div>
         <h2>History</h2>
+        <div>
+          {History.map((entry) =>  entry.infos.entry === 'income' ? <div>{ entry.title } income: {entry.infos.amount}</div> : <div>{ entry.title } expense: {entry.infos.amount}</div>,)}
+        </div>
       </div>
     );
   }
