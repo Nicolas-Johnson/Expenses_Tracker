80983c0438a5d4d4d5e27124b6348459ff725a22
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-13 13:28:55 -0300

    Balance change from class to func to be able to use hooks

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
 import Expenses from "./Expenses";
 import Income from "./Income";
 
-class Balance extends React.Component{
-  render() {
-    return(
-      <div>
-        <h2>Your Balance</h2>
-        <div>
-            <Income income="250" />
-            <Expenses expense="200" />
-        </div>
-      </div>
-    );
-  }
-}
+export default function Balance() {
+  const [balance, setBalance] = useState([]);
+  const [income, setIncome] = useState([]);
+  const [expense, setExpense] = useState([]);
 
-export default Balance;
+  const expenses = useSelector(state => state.ADD_TRANSACTION.EXPENSES);
+  const incomes = useSelector(state => state.ADD_TRANSACTION.INCOMES);
+
+  function reduceTransactions(transaction) {
+    return transaction.reduce((acc, curr) => acc + curr.amount, 0);
+  };
+
+  useEffect(() => {
+    const expense = reduceTransactions(expenses);
+    const incame = reduceTransactions(incomes);
+    const balance = incame - expense;
+    //console.log(blc);
+    setBalance([balance]);
+    setIncome([incame]);
+    setExpense([expense]);
+  }, [expenses, incomes])
+
+  return(
+    <div>
+      <h2>Your Balance</h2>
+      <h3>R$
+        <span> { balance }</span>
+      </h3>
+      <div>
+          <Income income={ income } />
+          <Expenses expense={ expense } />
+      </div>
+    </div>
+  );
+}
